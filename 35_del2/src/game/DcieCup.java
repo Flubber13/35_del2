65560af85212771e2d1829e7f314216ae7459d14
Oliver Elleman Thomsen <Oliver.e.t@hotmail.com>
2015-11-01 16:17:35 +0100

    Imported GUI and modified diceCup

--- 35_del2/src/game/DcieCup.java
+++ 35_del2/src/game/DcieCup.java
@@ -2,4 +2,13 @@ package game;
 
 public class DcieCup {
 
+	Dice die1 = new Dice(6); 
+	Dice die2 = new Dice(6);
+
+	
+	public int roll(){
+		die1.roll();
+		die2.roll();
+		return die1.lastRoll + die2.lastRoll;
+	}
 }
