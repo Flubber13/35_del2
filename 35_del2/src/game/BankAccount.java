91ca37f0c628c8a051c14083bc9a5011cc6e2e75
Oliver Elleman Thomsen <Oliver.e.t@hotmail.com>
2015-11-03 12:05:40 +0100

    Modified the BankAccount class

--- 35_del2/src/game/BankAccount.java
+++ 35_del2/src/game/BankAccount.java
@@ -1,16 +1,62 @@
 package game;
 
 public class BankAccount {
-
-	static int player1Balance = 1000;
-	static int player2Balance = 1000;
 	
+	private int balance;
 	
-	public int player1Balance(){
-		return player1Balance();
+	public void bankAccount(int balance){
+		this.balance = balance;
 	}
 	
-	public int player2Balance(){
-		return player2Balance();
+	
+	public int getBalance(){
+		return balance;
 	}
+	
+	
+	public int addBalance(int balance){
+		if (this.balance+balance<0)
+			this.balance = 0;
+		else this.balance = this.balance+balance;
+		return balance;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	static int player1Balance = 1000;
+//	static int player2Balance = 1000;
+//	
+//	
+//	public int player1Balance(){
+//		return player1Balance();
+//	}
+//	
+//	public int player2Balance(){
+//		return player2Balance();
+//	}
 }
