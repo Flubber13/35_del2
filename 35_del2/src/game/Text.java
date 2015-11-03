0e7ec5577f01c08af8f10e58533ffeaf407bdb05
Oliver Elleman Thomsen <Oliver.e.t@hotmail.com>
2015-11-03 11:17:26 +0100

    Added more text Strings to text class

--- 35_del2/src/game/Text.java
+++ 35_del2/src/game/Text.java
@@ -8,15 +8,18 @@ public class Text {
 	
 	static String winner = " wins the game!";
 	
-	static String tile2 = "Tower +250";
-	static String tile3 = "Crater -100";
-	static String tile4 = "Palace gates +100";
-	static String tile5 = "Cold Desert -20";
-	static String tile6 = "Walled city +180";
-	static String tile7 = "Monestary 0";
-	static String tile8 = "Black cave -70";
-	static String tile9 = "Huts in the mountain +60";
-	static String tile10 = "The Werewall -80, Extra turn";
-	static String tile11 = "The pit -50";
-	static String tile12 = "Goldmine +650";
+	static String field2 = " landed on: Tower +250";
+	static String field3 = " landed on: Crater -100";
+	static String field4 = " landed on: Palace gates +100";
+	static String field5 = " landed on: Cold Desert -20";
+	static String field6 = " landed on: Walled city +180";
+	static String field7 = " landed on: Monestary 0";
+	static String field8 = " landed on: Black cave -70";
+	static String field9 = " landed on: Huts in the mountain +60";
+	static String field10 = " landed on: The Werewall -80, Extra turn";
+	static String field11 = " landed on: The pit -50";
+	static String field12 = " landed on: Goldmine +650";
+
+	static String[] field = {"","",field2,field3,field4,field5,field6,
+							field7,field8,field9,field10,field11,field12};
 }
