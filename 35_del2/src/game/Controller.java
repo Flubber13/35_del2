3fdfd3bfa55d5716734af14c30a97694879311d7
Oliver Elleman Thomsen <Oliver.e.t@hotmail.com>
2015-11-03 16:33:37 +0100

    Created Controller

--- 35_del2/src/game/Controller.java
+++ 35_del2/src/game/Controller.java
@@ -8,17 +8,50 @@ public class Controller {
 		new Controller().run();
 	}
 
+	Player player1, player2;
+	DiceCup dicecup;
+
 	public void run() {
-		
-		Player.player1 = GUI.getUserString(Text.enterName1);
-		Player.player2 = GUI.getUserString(Text.enterName2);
-		
-		GUI.addPlayer(Player.player1, Player.p1Balance);
-		GUI.addPlayer(Player.player2, Player.p2Balance);
-		
-		
-		
-		
+
+		player1 = new Player(1000);
+		player2 = new Player(1000);
+		dicecup = new DiceCup();
+
+		player1.setName(GUI.getUserString(Text.enterName1));
+		player2.setName(GUI.getUserString(Text.enterName2));
+
+		GUI.addPlayer(player1.getName(), player1.getAccount().getBalance());
+		GUI.addPlayer(player2.getName(), player2.getAccount().getBalance());
+		while(player1.getAccount().getBalance()<3000 && player2.getAccount().getBalance()<3000){
+			playerTurn(player1);
+			playerTurn(player2);
+		}
+
+		if (player1.getAccount().getBalance() >= 3000 && player2.getAccount().getBalance() < 3000)
+			GUI.showMessage(">>>" + player1.getName() + Text.winner + "<<<");
+
+		if (player2.getAccount().getBalance() >= 3000 && player1.getAccount().getBalance() < 3000)
+			GUI.showMessage(">>>" + player2.getName() + " wins the game!<<<");
+
+		else 
+			GUI.showMessage(">>>" + player1.getName() + " and " + player2.getName() + " Tied the game!<<<");
+
 	}
+
+
+	private void playerTurn(Player player) {
+		int fieldNo;
+		do{
+			GUI.showMessage(player.getName() + Text.roll );
+			fieldNo = dicecup.roll();
+			GUI.setDice(dicecup.getDie1().getLastRoll(), dicecup.getDie2().getLastRoll());
+			GUI.showMessage(player.getName() + Text.field[fieldNo]);
+			player.getAccount().addBalance(Fields.field[fieldNo]);
+
+			GUI.setBalance(player.getName(), player.getAccount().getBalance());
+		}while (fieldNo==10);
+	}
+
+
 }
 
