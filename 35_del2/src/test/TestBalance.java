c1a5a946dcc99f44a425f452f6c345e92926078b
Oliver Elleman Thomsen <Oliver.e.t@hotmail.com>
2015-11-05 00:21:35 +0100

    Added a test package with a TestBalance class that tests if a balance can be negative.

---
+++ 35_del2/src/test/TestBalance.java
@@ -0,0 +1,26 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+
+import game.Player;
+
+public class TestBalance {
+
+	private Player player;
+
+	
+	@Before
+	public void setUp() throws Exception {
+		player = new Player(1000);
+		player.getAccount().addBalance(-2000);		
+	}
+
+	@org.junit.Test
+	public void test() {
+		Assert.assertEquals(0, player.getAccount().getBalance());
+	}
+
+}
