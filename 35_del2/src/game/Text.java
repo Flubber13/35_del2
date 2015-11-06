ab0c92ade5e06d33aa2711f504277653a8fcfd63
Simon James Jensen <Simon James Jensen@10.16.111.49>
2015-11-06 09:30:50 +0100

    Add game field texts.

--- 35_del2/src/game/Text.java
+++ 35_del2/src/game/Text.java
@@ -1,24 +1,24 @@
 package game;
 
 public class Text {
-	static String enterName1 = "Player 1 enter name";
-	static String enterName2 = "Player 2 enter name";
+	static String enterName1 = "Spiller 1 indtast navn";
+	static String enterName2 = "Spiller 2 indtast navn";
 	
-	static String roll = " press OK to roll dice";	
+	static String roll = " Tryk OK for at kaste terninger";	
 	
-	static String winner = " wins the game!";
+	static String winner = " vinder spillet!";
 	
-	static String field2 = " landed on: Tower +250";
-	static String field3 = " landed on: Crater -100";
-	static String field4 = " landed on: Palace gates +100";
-	static String field5 = " landed on: Cold Desert -20";
-	static String field6 = " landed on: Walled city +180";
-	static String field7 = " landed on: Monestary 0";
-	static String field8 = " landed on: Black cave -70";
-	static String field9 = " landed on: Huts in the mountain +60";
-	static String field10 = " landed on: The Werewall -80, Extra turn";
-	static String field11 = " landed on: The pit -50";
-	static String field12 = " landed on: Goldmine +650";
+	static String field2 = " landede på: Tårnet - 'Rapunzel, Rapunzel, lad dit hår falde ned', lyder det fra jorden udenfor vinduet. 'Et øjeblik', svarer du i en knap nok overbevisende falsetstemme, mens du fæstner din blonde paryk og giver kneblen et par øve-sving. Et par minutter senere smider du den bevidstløse eventyrer på den støt stigende bunke af samme i hjørnet, og stikker hans penge i lommen. +250.";
+	static String field3 = " landede på: Krateret - Du ankommer til Krateret, bydelens populæreste kro. Mens du bevæger dig gennem menneskemængden forbi baren, på vej ud på toilettet, lægger du mærke til at stede hovedsageligt er frekventeret af velklædte mænd. Ude i båsen ser du et hul i den ene væg, med teksten '100 daler pr mand' ridset i træværket underneden. 10 minutter senere forlader du krateret 100 daler fattigere, men i et glorværdigt humør! -100";
+	static String field4 = " landede på: Paladsets porte -  Ædle Kong Haargarn har bekendtgjort, at der skal holdes mesterskab i konkurrencespisning af skidne æg lige udenfor paladset, så han kan stå på sin balkon og pege og grine. Kongen er lidt underlig, men whatever, skidne æg er dit game og du vinder med lethed konkurrencen.  +100.";
+	static String field5 = " landede på: Den Kolde Ørken - I din søgen har du formået at rode dig ud i en skidekold ørken. Efter mange timers vandren ser du en oase. Da du kommer nærmere viser det sig at oasen var et fatamorgana, og at du rent faktisk står i en telefonboks. Du benytter lejligheden til at ringe efter en taxa. -20.";
+	static String field6 = " landede på: Bymuren - Foran den enorme mur hiver du din lut frem over giver dig i kast med din fortolkning af 'Wonderwall' i håbet om at nogle af byens borgere deroppe vil betale dig for at holde op igen. En halv time senere er der gået sport i at kyle kobberstykker efter dig, og selvom du har erhvervet dig en del blå mærker, og muligvis en hjernerystelse har du reddet dig en god stak byttepenge. +180.";
+	static String field7 = " landede på: Klosteret - Af uforklarlige årsager beslutter du dig for at gå i kloster og tager et fattighedsløfte. Du holder præcis én eftermiddag og det sidste munkene ser til dig, er da du, så hurtigt som dine ben kan bære dig, forsvinder over horisonten med en af deres øltønder over skulderen. +0.";
+	static String field8 = " landede på: Den Sorte Grotte - Du stikker goblinen en nævefuld mønter og tager imod den lille pakke tryllestøv. Et par minutter senere sidder du ved dit bord i et mørkt hjørne og tager bane efter bane af det fortryllende stof. Du læner dig tilbage i sædet og snart svæver du væk. Bogstaveligt talt. -70.";
+	static String field9 = " landede på: Hytterne i Bjergne - Din afdøde tante har efterladt dig en forfalden hytte i bjergene. Den lader til at være hjemsted for en ekstremt sundhedsskadelig koloni af mugsporer. Du formår at sælge den som en timeshare til nogle turister fra landet og stikker 60 daler i lommen. Vi håber du er stolt af dig selv. +60.";
+	static String field10 = " landede på: Varulvenes Mur - Mens du ihærdigt forsøger at forhandle en sørgelig gammel butiksbestyrerinde ned på en pris du ærligt talt burde skamme dig over at foreslå, lægge mærke til en flok særdeles behårede væsener, der kommer gående tværs over markedspladsen. Du genkender dit engangsknald fra tidligere blandet dem og skynder dig væk så hurtigt at du glemme din pung på disken. -80, Ekstra tur.";
+	static String field11 = " landede på: Hullet - Du står i baren på kroen Det Bundløse Hul og er i gang med et klodset forsøg på at imponere kromutter, da en sortelver smyger sig op ad dig. 'Passer det at de halvlange fra Herredet, ikke er så halvlange endda?', spørger hun. Du har aldrig været kræsen, så du betaler for din mjød og går med sortelveren. Et par timer senere vågner du i et isbad, og ud fra det friske indsnit i din torso at dømme, er du ret sikker på, at du mangler en nyre. 50";
+	static String field12 = " landede på: Guldminen - Et bagholdsangreb! Den fesne troldkarl Safronmand's horder af hærgende hårker vælter ind fra alle sider og samtlige medlemmer af dit eventyrergruppe går bravt til modangreb. Undtagen dig. Du træder snarrådigt ind bag et træ og gemmer dig i en mose. Da slaget er ovre træder du frem fra dit gemmested og går i gang med at lænse dine døde kammerater for værdigenstande. Du er rig! +650.";
 
 	static String[] field = {"","",field2,field3,field4,field5,field6,
 							field7,field8,field9,field10,field11,field12};
